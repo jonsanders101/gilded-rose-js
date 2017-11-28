f683dcb3516032f0234ea0e8faceaf7e912a53e7
Jon Sanders <jonsanders101@aol.com>
2017-11-28 18:36:48 +0000

    Rename original foo method

--- spec/shop_spec.js
+++ spec/shop_spec.js
@@ -1,7 +1,7 @@
 describe("Shop", function () {
 
   describe('#updateQuality', function() {
-    it("should foo", function() {
+    it("should return list with item name", function() {
       const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
       const items = gildedRose.updateQuality();
       expect(items[0].name).toEqual("foo");
