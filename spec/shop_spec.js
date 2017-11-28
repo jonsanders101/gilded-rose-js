b21111b188f1fc92605bc57f4b5463308d93836d
Jon Sanders <jonsanders101@aol.com>
2017-11-28 18:31:10 +0000

    Move original foo test into shop spec

---
+++ spec/shop_spec.js
@@ -0,0 +1,10 @@
+describe("Shop", function () {
+
+  describe('#updateQuality', function() {
+    it("should foo", function() {
+      const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].name).toEqual("foo");
+    });
+  })
+});
