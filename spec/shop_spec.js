84e3325d69e722410cb2aac7f3251dab649b4771
Jon Sanders <jonsanders101@aol.com>
2017-11-28 18:48:36 +0000

    Clean-up spec with beforeEach hook

--- spec/shop_spec.js
+++ spec/shop_spec.js
@@ -1,14 +1,18 @@
 describe("Shop", function () {
 
   describe('#updateQuality', function() {
+
+    var gildedRose, items;
+
+    beforeEach(function(){
+      gildedRose = new Shop([ new Item("foo", 1, 0) ]);
+      items = gildedRose.updateQuality();
+    })
+
     it("should return list with item name", function() {
-      const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
-      const items = gildedRose.updateQuality();
       expect(items[0].name).toEqual("foo");
     });
     it('should not reduce quality to less than zero', function () {
-      const gildedRose = new Shop([ new Item("foo", 1, 0) ]);
-      const items = gildedRose.updateQuality();
       expect(items[0].quality).toEqual(0);
     })
   })
