dea86a6341192acca9eba12b1ffc98750af09aa9
Jon Sanders <jonsanders101@aol.com>
2017-11-28 22:23:16 +0000

    Test standard item reduces quality

--- spec/shop_spec.js
+++ spec/shop_spec.js
@@ -7,12 +7,25 @@ describe("Shop", function () {
     beforeEach(function(){
       mockItems =  [{name: "foo", sellIn: 1, quality: 1},
                     {name: "Sulfuras, Hand of Ragnaros", sellIn: 1, quality: 0},
-                    {name: "banana", sellIn: 1, quality: 0}];
+                    {name: "zero quality", sellIn: 1, quality: 0},
+                    {name: "expired", sellIn: 0, quality: 10},];
       gildedRose = new Shop(mockItems);
       items = gildedRose.updateQuality();
     });
     it("should not reduce quality below zero", function () {
       expect(items[2].quality).toEqual(0);
+    });
+    describe("given the item is not brie, sulfuras, backstage pass or conjured", function () {
+      describe("given the item is within its sell-by-date", function () {
+        it("item quality should be reduced by 1", function () {
+          expect(items[0].quality).toEqual(0);
+        });
+      });
+      describe("given the item has expired", function () {
+        it("should by two less than current quality", function () {
+          expect(items[3].quality).toEqual(8);
+        });
+      });
     });
     it("should return list with item name included", function() {
       expect(items[0].name).toEqual("foo");
