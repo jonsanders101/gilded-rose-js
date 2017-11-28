0c1621598d02c5ff92f1aa3d31b706ba3104d7b7
Jon Sanders <jonsanders101@aol.com>
2017-11-28 18:50:54 +0000

    Test updateQuality decreases days to expiration

--- spec/shop_spec.js
+++ spec/shop_spec.js
@@ -7,13 +7,16 @@ describe("Shop", function () {
     beforeEach(function(){
       gildedRose = new Shop([ new Item("foo", 1, 0) ]);
       items = gildedRose.updateQuality();
-    })
+    });
 
     it("should return list with item name", function() {
       expect(items[0].name).toEqual("foo");
     });
     it('should not reduce quality to less than zero', function () {
       expect(items[0].quality).toEqual(0);
-    })
+    });
+    it('should reduce days to sell by one', function () {
+      expect(items[0].sellIn).toEqual(0);
+    });
   })
 });
