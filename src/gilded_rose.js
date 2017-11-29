7638b4b360b3e21d7fa4e1131c9a986e3efa872c
Jon Sanders <jonsanders101@aol.com>
2017-11-29 09:42:25 +0000

    Remove if-statement branch

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -62,9 +62,7 @@ class Shop {
           this.items[i].quality = this.items[i].quality + 1;
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].qualityTomorrow();
-              }
             }
             if (this.items[i].sellIn < 6) {
               if (this.items[i].quality < 50) {
