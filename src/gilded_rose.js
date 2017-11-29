1b7c6a8b5985d3daea3f7f17b76cb07842effc02
Jon Sanders <jonsanders101@aol.com>
2017-11-29 09:49:46 +0000

    Replace decrease quality value with new method

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -73,7 +73,7 @@ class Shop {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].quality > 0) {
               if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
+                this.items[i].quality = this.items[i].qualityTomorrow();
               }
             }
           } else {
