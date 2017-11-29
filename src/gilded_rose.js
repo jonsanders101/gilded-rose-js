0bfaf3d78432ef685fbb564ed58b7f1bbe9fe131
Jon Sanders <jonsanders101@aol.com>
2017-11-29 09:42:55 +0000

    Remove if-statement branch

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -61,9 +61,7 @@ class Shop {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
                 this.items[i].quality = this.items[i].qualityTomorrow();
-            }
             if (this.items[i].sellIn < 6) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].qualityTomorrow();
