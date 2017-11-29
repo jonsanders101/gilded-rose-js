70e8cce06e0b6ad7b00ce2de511c5a881972b1fe
Jon Sanders <jonsanders101@aol.com>
2017-11-29 12:44:00 +0000

    Remove unnecessary Sulfuras mock item

--- spec/shop_spec.js
+++ spec/shop_spec.js
@@ -6,22 +6,24 @@ describe("Shop", function () {
 
     beforeEach(function(){
       mockItems =  [{name: "foo", sellIn: 1, quality: 1, qualityTomorrow: () => {return 0}},
-                    {name: "Sulfuras, Hand of Ragnaros", sellIn: 1, quality: 0},
                     {name: "zero quality", sellIn: 1, quality: 0, qualityTomorrow: () => {return 0}},
                     {name: "expired", sellIn: 0, quality: 10, qualityTomorrow: () => {return 8}},
                     {name: "Aged Brie", sellIn: 10, quality: 10, qualityTomorrow: () => {return 11}},
                     {name: "Aged Brie", sellIn: 10, quality: 50, qualityTomorrow: () => {return 50}},
+                    {name: "Aged Brie", sellIn: 0, quality: 20, qualityTomorrow: () => {return 22}},
                     {name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80, qualityTomorrow: () => {return 22}},
                     {name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 11, quality: 20, qualityTomorrow: () => {return 21}},
                     {name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 10, quality: 20, qualityTomorrow: () => {return 22}},
                     {name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 5, quality: 20, qualityTomorrow: () => {return 23}},
-                    {name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 0, quality: 20, qualityTomorrow: () => {return 0}},
-                    {name: "Aged Brie", sellIn: 0, quality: 20, qualityTomorrow: () => {return 22}}];
+                    {name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 0, quality: 20, qualityTomorrow: () => {return 0}}];
       gildedRose = new Shop(mockItems);
       items = gildedRose.updateQuality();
     });
+    it("should return list with item name included", function() {
+      expect(items[0].name).toEqual("foo");
+    });
     it("should not reduce quality below zero", function () {
-      expect(items[2].quality).toEqual(0);
+      expect(items[1].quality).toEqual(0);
     });
     describe("given the item is not brie, sulfuras, backstage pass or conjured", function () {
       describe("given the item is within its sell-by-date", function () {
@@ -30,21 +32,21 @@ describe("Shop", function () {
         });
       });
       describe("given the item has expired", function () {
-        it("should by two less than current quality", function () {
-          expect(items[3].quality).toEqual(8);
+        it("should be two less than current quality", function () {
+          expect(items[2].quality).toEqual(8);
         });
       });
     });
     describe("given AgedBrie", function () {
       it("should increase quality value", function () {
-        expect(items[4].quality).toEqual(11);
+        expect(items[3].quality).toEqual(11);
       });
       it("should not increase quality value above 50", function () {
-        expect(items[5].quality).toEqual(50);
+        expect(items[4].quality).toEqual(50);
       });
       describe("given it is passed expiry", function () {
         it("should increase quality value by 2", function () {
-          expect(items[11].quality).toEqual(22);
+          expect(items[5].quality).toEqual(22);
         });
       });
     });
@@ -80,9 +82,6 @@ describe("Shop", function () {
           expect(items[10].quality).toEqual(0);
         });
       });
-    });
-    it("should return list with item name included", function() {
-      expect(items[0].name).toEqual("foo");
     });
   });
 });
