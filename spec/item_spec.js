da0b9f5a662ad554eec4728b884d6e1f9efcc74c
Jon Sanders <jonsanders101@aol.com>
2017-11-30 22:10:57 +0000

    Transfer tests for BackStagePass

--- spec/item_spec.js
+++ spec/item_spec.js
@@ -30,6 +30,34 @@ describe('AgedBrie', function () {
   });
 });
 
+describe('BackStagePass', function () {
+  describe('given that the sellIn value is greater than 10', function () {
+    it('increases quality value by 1', function () {
+      var testPass = new BackStagePass('pass', 11, 20);
+      expect(testPass.itemTomorrow().quality).toEqual(21);
+    });
+  });
+  describe('given that the sellIn value is 10 or less', function () {
+    it('increases quality value by 2', function () {
+      var testPass = new BackStagePass('pass', 10, 20);
+      expect(testPass.itemTomorrow().quality).toEqual(22);
+    });
+  });
+  describe('given that the sellIn value is 5 or less', function () {
+    it('increases quality value by 3', function () {
+      var testPass = new BackStagePass('pass', 5, 20);
+      expect(testPass.itemTomorrow().quality).toEqual(23);
+    });
+  });
+  describe('given that the sellIn value is 0 or less', function () {
+    it('sets quality value to 0', function () {
+      var testPass = new BackStagePass('pass', 0, 20);
+      expect(testPass.itemTomorrow().quality).toEqual(0);
+    });
+  });
+
+});
+
 describe("Item", function () {
 
   var testItem;
@@ -63,32 +91,6 @@ describe("Item", function () {
         });
       });
 
-      describe("given BackStagePass", function () {
-        describe("given there are more than 10 days until expiry", function () {
-          it("should increase by 1", function () {
-            var testItem = new BackStagePass('pass', 11, 20);
-            expect(testItem.itemTomorrow().quality).toEqual(21);
-          });
-        });
-        describe("given 10 or fewer days until sell-by date", function () {
-          it("should increase by 2", function () {
-            var testItem = new BackStagePass('pass', 10, 20);
-            expect(testItem.itemTomorrow().quality).toEqual(22);
-          });
-        });
-        describe("given 5 or fewer days until sell-by date", function () {
-          it("should increase by 3", function () {
-            var testItem = new BackStagePass('pass', 5, 20);
-            expect(testItem.itemTomorrow().quality).toEqual(23);
-          });
-        });
-        describe("given that concert has happened", function () {
-          it("should be zero", function () {
-            var testItem = new BackStagePass('pass', 0, 20);
-            expect(testItem.itemTomorrow().quality).toEqual(0);
-          });
-        })
-      });
     });
   });
 });
