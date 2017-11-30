881e21b9bb45845d40541e64f434e963821064b8
Jon Sanders <jonsanders101@aol.com>
2017-11-30 09:49:20 +0000

    Refactor shop spec

--- spec/shop_spec.js
+++ spec/shop_spec.js
@@ -1,19 +1,20 @@
 describe("Shop", function () {
 
   describe('#updateStock', function () {
-    var shop, items, mockItems, itemsTomorrow, itemSpy, itemTomorrow;
+    var itemTomorrow, mockItem, shop;
+
+    beforeEach(function() {
+      itemTomorrow = {};
+      mockItem = {itemTomorrow: () => {return itemTomorrow;}};
+      shop = new Shop([mockItem]);
+    });
 
     it("should call #itemTomorrow on each item", function () {
-      itemSpy = {itemTomorrow: () => {return {};}};
-      spyOn(itemSpy, "itemTomorrow");
-      shop = new Shop([itemSpy]);
+      spyOn(mockItem, "itemTomorrow");
       shop.updateStock(shop.items);
-      expect(itemSpy.itemTomorrow).toHaveBeenCalled();
+      expect(mockItem.itemTomorrow).toHaveBeenCalled();
     });
     it("should return a list of updated items", function () {
-      itemTomorrow = {name: "foo", sellIn: 0, quality: 0};
-      mockItem = {name: "foo", sellIn: 1, quality: 1, qualityTomorrow: () => {return 0;}, itemTomorrow: () => {return itemTomorrow;}};
-      shop = new Shop([mockItem]);
       itemsTomorrow = shop.updateStock(shop.items);
       expect(itemsTomorrow).toEqual([itemTomorrow]);
     });
