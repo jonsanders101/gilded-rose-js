b9ee5ceaddc789f0c32783b2891c0a2c97819285
Jon Sanders <jonsanders101@aol.com>
2017-11-30 09:38:53 +0000

    Refactor shop spec

--- spec/shop_spec.js
+++ spec/shop_spec.js
@@ -1,25 +1,20 @@
 describe("Shop", function () {
 
   describe('#updateStock', function () {
-    var gildedRose, items, mockItems, itemsTomorrow, itemSpy, itemTomorrow;
+    var shop, items, mockItems, itemsTomorrow, itemSpy, itemTomorrow;
 
-    beforeEach(function(){
-      itemTomorrow = {name: "foo", sellIn: 0, quality: 0};
-      itemSpy = {name: "foo", sellIn: 1, quality: 1, qualityTomorrow: () => {return 0;}, itemTomorrow: () => {return {};}};
-      spyOn(itemSpy, "itemTomorrow");
-      mockItems =  [itemSpy];
-      gildedRose = new Shop(mockItems);
-      itemsTomorrow = gildedRose.updateStock(mockItems);
-      console.log(itemsTomorrow);
-    });
     it("should call #itemTomorrow on each item", function () {
+      itemSpy = {itemTomorrow: () => {return {};}};
+      spyOn(itemSpy, "itemTomorrow");
+      shop = new Shop([itemSpy]);
+      shop.updateStock(shop.items);
       expect(itemSpy.itemTomorrow).toHaveBeenCalled();
     });
     it("should return a list of updated items", function () {
       itemTomorrow = {name: "foo", sellIn: 0, quality: 0};
-      mockItem = [{name: "foo", sellIn: 1, quality: 1, qualityTomorrow: () => {return 0;}, itemTomorrow: () => {return itemTomorrow;}}];
-      gildedRose = new Shop(mockItem);
-      itemsTomorrow = gildedRose.updateStock(mockItem);
+      mockItem = {name: "foo", sellIn: 1, quality: 1, qualityTomorrow: () => {return 0;}, itemTomorrow: () => {return itemTomorrow;}};
+      shop = new Shop([mockItem]);
+      itemsTomorrow = shop.updateStock(shop.items);
       expect(itemsTomorrow).toEqual([itemTomorrow]);
     });
   });
