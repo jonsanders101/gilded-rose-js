01ebdfa1495cdac3a13fe5891abbbcc6877e02c0
Jon Sanders <jonsanders101@aol.com>
2017-11-30 09:21:56 +0000

    Remove redundant mock items

--- spec/shop_spec.js
+++ spec/shop_spec.js
@@ -7,26 +7,20 @@ describe("Shop", function () {
       itemTomorrow = {name: "foo", sellIn: 0, quality: 0};
       itemSpy = {name: "foo", sellIn: 1, quality: 1, qualityTomorrow: () => {return 0;}, itemTomorrow: () => {return {};}};
       spyOn(itemSpy, "itemTomorrow");
-
-      mockItems =  [{name: "foo", sellIn: 1, quality: 1, qualityTomorrow: () => {return 0}, itemTomorrow: () => {return itemTomorrow}},
-                    {name: "zero quality", sellIn: 1, quality: 0, qualityTomorrow: () => {return 0}, itemTomorrow: () => {return {name: "zero quality", sellIn: 0, quality: 0}}},
-                    {name: "expired", sellIn: 0, quality: 10, qualityTomorrow: () => {return 8}, itemTomorrow: () => {return {name: "expired", sellIn: 0, quality: 8}}},
-                    {name: "Aged Brie", sellIn: 10, quality: 10, qualityTomorrow: () => {return 11}, itemTomorrow: () => {return {name: "Aged Brie", sellIn: 9, quality: 11}}},
-                    {name: "Aged Brie", sellIn: 10, quality: 50, qualityTomorrow: () => {return 50}, itemTomorrow: () => {return {name: "Aged Brie", sellIn: 9, quality: 50}}},
-                    {name: "Aged Brie", sellIn: 0, quality: 20, qualityTomorrow: () => {return 22}, itemTomorrow: () => {return {name: "Aged Brie", sellIn: -1, quality: 22}}},
-                    {name: "Sulfuras, Hand of Ragnaros", sellIn: 1, quality: 80, qualityTomorrow: () => {return 80}, itemTomorrow: () => {return {name: "Sulfuras, Hand of Ragnaros", sellIn: 1, quality: 80}}},
-                    {name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 11, quality: 20, qualityTomorrow: () => {return 21}, itemTomorrow: () => {return {name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 10, quality: 21}}},
-                    {name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 10, quality: 20, qualityTomorrow: () => {return 22}, itemTomorrow: () => {return {name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 9, quality: 22}}},
-                    {name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 5, quality: 20, qualityTomorrow: () => {return 23}, itemTomorrow: () => {return {name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 4, quality: 23}}},
-                    {name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 0, quality: 20, qualityTomorrow: () => {return 0}, itemTomorrow: () => {return {name: "Backstage passes to a TAFKAL80ETC concert", sellIn: -1, quality: 0}}},
-                    itemSpy];
+      mockItems =  [itemSpy];
       gildedRose = new Shop(mockItems);
       itemsTomorrow = gildedRose.updateStock(mockItems);
       console.log(itemsTomorrow);
     });
-
     it("should call #itemTomorrow on each item", function () {
       expect(itemSpy.itemTomorrow).toHaveBeenCalled();
+    });
+    it("should return a list of updated items", function () {
+      itemTomorrow = {name: "foo", sellIn: 0, quality: 0};
+      mockItem = [{name: "foo", sellIn: 1, quality: 1, qualityTomorrow: () => {return 0;}, itemTomorrow: () => {return itemTomorrow;}}];
+      gildedRose = new Shop(mockItem);
+      itemsTomorrow = gildedRose.updateStock(mockItem);
+      expect(itemsTomorrow).toEqual([itemTomorrow]);
     });
   });
 });
