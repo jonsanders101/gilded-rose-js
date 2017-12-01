ec49f42128efb0e1f622b835d1a5fb5a4d28ed33
Jon Sanders <jonsanders101@aol.com>
2017-12-01 21:08:35 +0000

    Refactor ConjuredItem spec

--- spec/ConjuredItem_spec.js
+++ spec/ConjuredItem_spec.js
@@ -11,11 +11,16 @@ describe('ConjuredItem', function () {
       expect(testConjured.itemTomorrow().quality).toEqual(8);
     });
   });
-  describe('given that the sellIn value is 0 or less', function () {
+  describe('given that the sellIn value is 0', function () {
     it('increases quality value by 4', function () {
       testConjured.sellIn = 0;
       expect(testConjured.itemTomorrow().quality).toEqual(6);
     });
   });
-
+  describe('given that the sellIn value is less than 0', function () {
+    it('increases quality value by 4', function () {
+      testConjured.sellIn = -1;
+      expect(testConjured.itemTomorrow().quality).toEqual(6);
+    });
+  });
 });
