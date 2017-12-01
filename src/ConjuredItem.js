1933903668a52c11a38408189d2743579b4b6ade
Jon Sanders <jonsanders101@aol.com>
2017-12-01 22:32:06 +0000

    Remove more unnecessary parentheses

--- src/ConjuredItem.js
+++ src/ConjuredItem.js
@@ -1,7 +1,7 @@
 (function(exports){
   class ConjuredItem extends StandardItem {
     _calculateDepreciation () {
-      return this.sellIn <= 0 ? (4):(2);
+      return this.sellIn <= 0 ? 4 : 2;
     }
   }
   exports.ConjuredItem = ConjuredItem;
