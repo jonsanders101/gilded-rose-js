f0786a5d59c2bd8324ffc7cde2a4bfc27e67fdcf
Jon Sanders <jonsanders101@aol.com>
2017-12-01 22:26:18 +0000

    Refactor StandardItem spec

--- spec/StandardItem_spec.js
+++ spec/StandardItem_spec.js
@@ -1,35 +1,28 @@
 describe('StandardItem', function () {
-
-  var testItem;
-
-  beforeEach(function () {
-    testItem = new StandardItem('apple', 3, 0);
-  });
-
   describe('#itemTomorrow', function () {
     it('reduces sellIn value by 1', function () {
+      let testItem = new StandardItem('apple', 3, 0);
       expect(testItem.itemTomorrow().sellIn).toEqual(2);
     });
     it('reduces quality value to a minimum of 0', function () {
+      let testItem = new StandardItem('apple', 3, 0);
       expect(testItem.itemTomorrow().quality).toEqual(0);
     });
     describe('given that the sellIn value is greater than 0', function () {
       it('reduces quality value by 1', function () {
-        testItem.quality = 3;
+        let testItem = new StandardItem('apple', 3, 3);
         expect(testItem.itemTomorrow().sellIn).toEqual(2);
       });
     });
     describe('given that the sellIn value is 0', function () {
       it('increases quality value by 2', function () {
-        testItem.quality = 3;
-        testItem.sellIn = 0;
+        let testItem = new StandardItem('apple', 0, 3);
         expect(testItem.itemTomorrow().quality).toEqual(1);
       });
     });
     describe('given that the sellIn value is less than 0', function () {
       it('increases quality value by 2', function () {
-        testItem.quality = 3;
-        testItem.sellIn = -1;
+        let testItem = new StandardItem('apple', -1, 3);
         expect(testItem.itemTomorrow().quality).toEqual(1);
       });
     });
