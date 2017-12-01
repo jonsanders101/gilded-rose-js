e3835ce40e9c9781e660e2a7f06aa98f74ba28e6
Jon Sanders <jonsanders101@aol.com>
2017-12-01 22:31:26 +0000

    Remove unnecessary parentheses

--- src/AgedBrie.js
+++ src/AgedBrie.js
@@ -1,7 +1,7 @@
 (function(exports){
   class AgedBrie extends StandardItem {
     _calculateDepreciation () {
-      return this.sellIn <= 0 ? (-2):(-1);
+      return this.sellIn <= 0 ? -2 : -1;
     }
   }
   exports.AgedBrie = AgedBrie;
--- src/BackStagePass.js
+++ src/BackStagePass.js
@@ -2,14 +2,14 @@
   class BackStagePass extends StandardItem {
     _calculateDepreciation () {
       switch (true) {
-        case (this.sellIn <= 0):
+        case this.sellIn <= 0:
           return this.quality;
-        case (this.sellIn <= 5):
-          return (-3);
-        case (this.sellIn <= 10):
-          return (-2);
+        case this.sellIn <= 5:
+          return -3;
+        case this.sellIn <= 10:
+          return -2;
         default:
-          return (-1);
+          return -1;
       }
     }
   }
