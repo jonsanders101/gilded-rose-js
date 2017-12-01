e4e1f30e2299f1c3ab81536fb70a16b0f646d8f9
Jon Sanders <jonsanders101@aol.com>
2017-12-01 22:09:23 +0000

    Remove beforeEach block for readability

--- spec/AgedBrie_spec.js
+++ spec/AgedBrie_spec.js
@@ -1,22 +1,22 @@
 describe('AgedBrie', function () {
   describe('#itemTomorrow', function () {
     it('increases quality value by 1', function () {
-      var testBrie = new AgedBrie('brie', 1, 1);
+      let testBrie = new AgedBrie('brie', 1, 1);
       expect(testBrie.itemTomorrow().quality).toEqual(2);
     });
     it('increases quality value to a maximum of 50', function () {
-      var testBrie = new AgedBrie('brie', 3, 50);
+      let testBrie = new AgedBrie('brie', 3, 50);
       expect(testBrie.itemTomorrow().quality).toEqual(50);
     });
     describe('given that the sellIn value is 0 or less', function () {
       it('increases quality by 2', function () {
-        var testBrie = new AgedBrie('brie', 0, 1);
+        let testBrie = new AgedBrie('brie', 0, 1);
         expect(testBrie.itemTomorrow().quality).toEqual(3);
       });
     });
     describe('given that the sellIn value is less than 0', function () {
       it('increases quality by 2', function () {
-        var testBrie = new AgedBrie('brie', -1, 1);
+        let testBrie = new AgedBrie('brie', -1, 1);
         expect(testBrie.itemTomorrow().quality).toEqual(3);
       });
     });
--- spec/ConjuredItem_spec.js
+++ spec/ConjuredItem_spec.js
@@ -1,25 +1,19 @@
 describe('ConjuredItem', function () {
-
-  var testConjured;
-
-  beforeEach(function () {
-    testConjured = new ConjuredItem('conjured apple', 3, 10);
-  });
-
   describe('given that the sellIn value is greater than 0', function () {
     it('reduces quality value by 2', function () {
+      let testConjured = new ConjuredItem('conjured apple', 3, 10);
       expect(testConjured.itemTomorrow().quality).toEqual(8);
     });
   });
   describe('given that the sellIn value is 0', function () {
     it('increases quality value by 4', function () {
-      testConjured.sellIn = 0;
+      let testConjured = new ConjuredItem('conjured apple', 0, 10);
       expect(testConjured.itemTomorrow().quality).toEqual(6);
     });
   });
   describe('given that the sellIn value is less than 0', function () {
     it('increases quality value by 4', function () {
-      testConjured.sellIn = -1;
+      let testConjured = new ConjuredItem('conjured apple', -1, 10);
       expect(testConjured.itemTomorrow().quality).toEqual(6);
     });
   });
