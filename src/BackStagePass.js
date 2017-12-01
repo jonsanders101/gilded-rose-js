68fb5c9c16d65ade62a8458e6c177dc3656e321c
Jon Sanders <jonsanders101@aol.com>
2017-12-01 20:56:11 +0000

    Replace if-else with a switch statement

--- src/BackStagePass.js
+++ src/BackStagePass.js
@@ -1,16 +1,17 @@
 (function(exports) {
   class BackStagePass extends StandardItem {
     _calculateDepreciation () {
-      if (this.sellIn === 0) {
-        return this.quality;
-      } else if (this.sellIn <= 5) {
-        return (-3);
-      } else if (this.sellIn <= 10) {
-        return (-2);
-      } else {
-        return (-1);
+      switch (true) {
+        case (this.sellIn === 0):
+          return this.quality;
+        case (this.sellIn <= 5):
+          return (-3);
+        case (this.sellIn <= 10):
+          return (-2);
+        default:
+          return (-1);
       }
     }
   }
   exports.BackStagePass = BackStagePass;
-}(this))
+}(this));
